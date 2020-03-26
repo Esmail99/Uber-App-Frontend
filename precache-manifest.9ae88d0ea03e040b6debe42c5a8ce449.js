e45340b2c4cb7658030fd49ba36f0a86b80cd9bb
Esmail <elmoussel12@gmail.com>
2020-03-26 07:52:06 +0200

    Updates

---
+++ precache-manifest.9ae88d0ea03e040b6debe42c5a8ce449.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cf774ccb891a0a213e0ec54d94aaa5c",
+    "url": "/Uber-App-Frontend/index.html"
+  },
+  {
+    "revision": "b1ab63150b9a7b1ba5b9",
+    "url": "/Uber-App-Frontend/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "34b25a15c38db6cfe859",
+    "url": "/Uber-App-Frontend/static/css/main.f7f5dc0f.chunk.css"
+  },
+  {
+    "revision": "b1ab63150b9a7b1ba5b9",
+    "url": "/Uber-App-Frontend/static/js/2.de02e5f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Uber-App-Frontend/static/js/2.de02e5f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34b25a15c38db6cfe859",
+    "url": "/Uber-App-Frontend/static/js/main.4c8eb832.chunk.js"
+  },
+  {
+    "revision": "684215acac3d3c3b189b",
+    "url": "/Uber-App-Frontend/static/js/runtime-main.46806e6b.js"
+  }
+]);
